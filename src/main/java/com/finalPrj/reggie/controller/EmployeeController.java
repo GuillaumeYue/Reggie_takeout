60cffa23a3a25ef24b76a2ac0d5ba93ea69f73e3
Han Yue <alexyuehan@gmail.com>
2025-03-26 22:24:42 -0400

    Backend Login & Logout

--- src/main/java/com/finalPrj/reggie/controller/EmployeeController.java
+++ src/main/java/com/finalPrj/reggie/controller/EmployeeController.java
@@ -1,10 +1,19 @@
 package com.finalPrj.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.finalPrj.reggie.common.R;
+import com.finalPrj.reggie.entity.Employee;
 import com.finalPrj.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -12,4 +21,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    //员工登录
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        //1.将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2.根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //3.如果没有查询到则返回登陆失败结果
+        if (emp == null) {
+            return R.error("Login Failed");
+        }
+
+        //4.密码比对，如果不一致则返回登陆失败结果
+        if (!emp.getPassword().equals(password)) {
+            return R.error("Login Failed");//SpringBoot的MCV框架将R对象转成json文件，从login的response能看见
+        }
+
+        //5.查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0) {
+            log.info("Employee Status：{}", emp.getStatus());
+
+            return R.error("Account is locked");
+        }
+
+        //6.登录成功，将员工id存入Session并返回登陆成功结果
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+
+    //员工推出
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request) {
+        //清理Seesion中保存的当前登录员工id
+        request.getSession().removeAttribute("employee");
+        return R.success("Logout Success");
+    }
 }
