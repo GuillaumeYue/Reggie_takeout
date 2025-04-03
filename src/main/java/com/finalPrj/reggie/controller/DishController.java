c669e04f88379805d2b78615c2148f8061a09a42
Han Yue <alexyuehan@gmail.com>
2025-04-03 00:00:19 -0400

    Dish pagination

--- src/main/java/com/finalPrj/reggie/controller/DishController.java
+++ src/main/java/com/finalPrj/reggie/controller/DishController.java
@@ -1,17 +1,23 @@
 package com.finalPrj.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.finalPrj.reggie.common.R;
 import com.finalPrj.reggie.dto.DishDto;
+import com.finalPrj.reggie.entity.Category;
+import com.finalPrj.reggie.entity.Dish;
+import com.finalPrj.reggie.mapper.DishMapper;
+import com.finalPrj.reggie.service.CategoryService;
 import com.finalPrj.reggie.service.DishFlavorService;
 import com.finalPrj.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -26,6 +32,9 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * 新增菜品
      * @param dishDto
@@ -38,5 +47,53 @@ public class DishController {
         dishService.saveWithFlavor(dishDto);
 
         return R.success("Add Dish Success");
+    }
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        //构造分页构造器对象
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        //条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(name != null, Dish:: getName, name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        //执行分页查询
+        dishService.page(pageInfo, queryWrapper);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);//普通属性拷贝上来
+
+            //查数据库，单独把categoryname查上来
+            Long categoryId = item.getCategoryId(); //分类id
+            Category category = categoryService.getById(categoryId);
+
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName); //categoryname赋好值了
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
     }
 }
