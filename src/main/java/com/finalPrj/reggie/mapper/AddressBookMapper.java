e27bcccb4bfdb5805cc76dcce3204ac6a58887e7
Han Yue <alexyuehan@gmail.com>
2025-04-11 00:25:10 -0400

    SMS Validation

---
+++ src/main/java/com/finalPrj/reggie/mapper/AddressBookMapper.java
@@ -0,0 +1,13 @@
+package com.finalPrj.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.finalPrj.reggie.entity.AddressBook;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+
+}
